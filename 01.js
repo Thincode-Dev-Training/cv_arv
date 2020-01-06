1a00756e1a221da5532945c236b85ff01ae7fe3e
Alfredo Renteria <alfredo.renteria@thincode.com>
2020-01-06 15:42:54 -0600

    add styles and create all pages

--- 01.js
+++ 01.js
@@ -1,6 +1,43 @@
 const app = new Vue({
   el: '#app',
   data: {
-    name: 'Alfredo Rentería Villanueva'
+    name: 'Alfredo Rentería Villanueva',
+    about_me: 'About me'
   }
+});
+
+$("#circuit_design").ProgressBarWars({
+  porcentaje:75,
+  tiempo: 450,
+  color: "#00eaff"
+});
+$("#pcb_layout").ProgressBarWars({
+  porcentaje:80,
+  tiempo: 840,
+  color: "#fb00ff",
+});
+$("#web_development").ProgressBarWars({
+  porcentaje:50,
+  tiempo: 450,
+  color: "#09ff00",
+});
+$("#programming").ProgressBarWars({
+  porcentaje:85,
+  tiempo: 1150,
+  color: "#ffffff",
+});
+$("#project_management").ProgressBarWars({
+  porcentaje:65,
+  tiempo: 950,
+  color: "#00eaff",
+});
+$("#team_work").ProgressBarWars({
+  porcentaje:100,
+  tiempo: 1400,
+  color: "#fb00ff",
+});
+$("#leadership").ProgressBarWars({
+  porcentaje:70,
+  tiempo: 1000,
+  color: "#09ff00",
 });
